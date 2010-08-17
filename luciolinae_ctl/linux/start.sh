81d307766a01e6d1155f037ddfdb0e38bb6a660a
Kiberpipa <pipa@lab132.(none)>
2010-08-17 12:28:51 +0200

    more bugfixes to start script
    	modified:   linux/start.sh

--- luciolinae_ctl/linux/start.sh
+++ luciolinae_ctl/linux/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # launch pd
-pd_patch_path=$PWD/`dirname $0`/../bin/data/pdstuff/_main.pd
+pd_patch_path=`dirname $0`/../bin/data/pdstuff/_main.pd
 #pd -nogui -oss -blocksize 1024 ~/pd-test.pd &
 # echo ~/code/puredata-0.42.5/bin/pd -nogui -sleepgrain 2 -verbose -alsa $pd_patch_path &
 pd -oss $pd_patch_path &
@@ -13,7 +13,7 @@ sleep 5
 
 # now open control app
 echo starting luciolinae_ctl
-ctl_app_path=$PWD/`dirname $0`/../bin
+ctl_app_path=`dirname $0`/../bin
 cd $ctl_app_path
 echo $ctl_app_path
 ls
